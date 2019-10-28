07ebe936564c05c009a5dd385d9f2b8883d7594b
Rodrigo Pierini <rodrigorivelino656@gmail.com>
2019-10-28 20:48:33 -0300

    não aguento mais

--- resources/views/medicacaos/index.blade.php
+++ resources/views/medicacaos/index.blade.php
@@ -3,11 +3,10 @@
 @section('content')
     <section class="content-header d-flex">
         <div>
-            <h1 class="pull-left">Medicação</h1>
+            <h1 class="pull-left">Medicamentos</h1>
             <p class="pull-left">{!!  (is_null($pet->nome))!!}</p>
         </div>
 
-        <h1 class="pull-left">Medicamentos</h1>
         <h1 class="ml-auto">
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('medicacaos.create', [$pet->id]) !!}"><i class="fas fa-plus"></i></a>
         </h1>
